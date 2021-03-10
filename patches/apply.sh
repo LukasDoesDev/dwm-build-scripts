7c31ba0b21f6118f3c56bef82bdbf6a2865ed074
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-03-10 11:38:02 +0200

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,6 +2,11 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+ls $SCRIPT_DIR -al | grep ".diff"
+if [ "$?" != "0" ]; then
+  exit 0
+fi
+
 for file in $SCRIPT_DIR/*.diff
 do
   patch < $file
